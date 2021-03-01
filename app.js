dcc955fb0ce3b2f62416050341554e4691704791
Vladimir Volkov <00010419@wiut.uz>
2021-03-01 12:15:32 +0500

    add layout

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(8000, err => {
+    if (err) throw err
+    console.log('App is running ...')
+})
