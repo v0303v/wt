446504a6e8f4060b2ebffa4f5df38fffa07832d5
00010419 <00010419.wiut@gmail.com>
2021-03-08 12:36:06 +0500

    add style to create page and all page

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/create_note', (req, res) => {
+    res.render('create_note')
+})
+
+app.get('/notes', (req, res) => {
+    const notes = ['Note Uno','Note Dos','Note Tres']
+    res.render('all_notes', { notes: notes})
+})
+
+app.listen(8000, err => {
+    if (err) throw err
+    console.log('App is running ...')
+})
