0aa8f209d11947d90ef487ec3d23b7e1f2899f69
Vladimir Volkov <00010419@wiut.uz>
2021-03-15 13:00:55 +0500

    add additional stuff

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'pug')
-
 app.use('/static', express.static('public'))
+
+const notes = require('./db')
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -13,18 +14,14 @@ app.get('/create_note', (req, res) => {
     res.render('create_note')
 })
 
-<<<<<<< HEAD
 app.get('/notes', (req, res) => {
-    const notes = ['Note Uno','Note Dos','Note Tres']
-    res.render('all_notes', { notes: notes})
+    res.render('all_notes', {notes : notes})
 })
 
 app.get('/notes/detail', (req, res) => {
     res.render('notes_detail')
 })
 
-=======
->>>>>>> aa8e08ff6c416c7e9895fdd60f0d20d0c04620f1
 app.listen(8000, err => {
     if (err) throw err
     console.log('App is running ...')
---
+++ db.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'Title of a random note',
+        desc: 'Description!',    
+    },
+
+    {
+        id: 2,
+        title: 'Title of a note Number TWO!',
+        desc: 'Description!', 
+    },
+
+    {
+        id: 3,
+        title: 'Title of a note Number THREE!',
+        desc: 'Description!', 
+    }
+]
